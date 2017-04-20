b09bf5134b3904ea152f6b0431fad2e7e24e42d2
umasuo <liuquan89@gmail.com>
2017-04-20 19:58:29 +0800

    add order created publisher

---
+++ src/main/java/io/reactivesw/order/infrastructure/configuration/EventConfig.java
@@ -0,0 +1,19 @@
+package io.reactivesw.order.infrastructure.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Event config.
+ */
+@Configuration
+@Data
+public class EventConfig {
+
+  /**
+   * Google cloud project id.
+   */
+  @Value("${io.reactivesw.message.google.project.id}")
+  private String googleCloudProjectId;
+}
